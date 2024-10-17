8e8b455b7bc966c5cd5b3efa18f2f54f17dc0aa8
Lalit Patil <lalitbansipatil1702@gmail.com>
2024-10-17 16:32:16 +0530

    add models

---
+++ src/main/java/com/example/VacciNation/model/Dose.java
@@ -0,0 +1,33 @@
+package com.example.VacciNation.model;
+
+import com.example.VacciNation.Enum.VaccineBrand;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Dose {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Enumerated(value = EnumType.STRING)
+    private VaccineBrand vaccineBrand;
+
+    private boolean taken;
+
+    private String serialNumber;
+
+    @CreationTimestamp
+    private Date dateOfVaccination;
+}
